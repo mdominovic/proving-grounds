6fb70c01309fca796468b442644d313ea15186e5
Mislav <mislav.dominovic@rt-rk.com>
2020-03-18 17:13:01 +0100

    reworked perms without itertools and possibility of X number of elements for permuting

--- python/permutations/permutations.py
+++ python/permutations/permutations.py
@@ -1,13 +1,16 @@
-import itertools
-
 def permute(input_list):
-    result_list = []
-    permuted_list = list(itertools.permutations(input_list))
-    for i in list(permuted_list):
-        result_list.append(list(i))
-    return result_list
+    if len(input_list) == 1:
+        return [input_list]
+    else:
+        output_list = []
+        for i in range(0, len(input_list)):
+            element = input_list[i]
+            rest = input_list[:i] + input_list[i+1:]
+            for var in permute(rest):
+                output_list.append([element] + var)
+        return output_list
 
 
 if __name__ == "__main__":
-    input_list = [6, 5, 4]
+    input_list = [1, 2, 3]
     output = permute(input_list)
